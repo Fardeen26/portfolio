c7ace84e863021b79c41886aab74eeb119f517c9
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2025-01-02 22:08:07 +0530

    fix: minor changes

--- src/app/about/components/About.tsx
+++ src/app/about/components/About.tsx
@@ -9,83 +9,89 @@ import Title from '@/components/ui/Title';
 
 const About = () => {
     const [isMore, setIsMore] = useState<boolean>(false);
-  return (
-    <div className='w-full px-64 max-[1285px]:px-52 max-lg:px-4 max-sm:px-5 flex flex-col items-center mt-4 pb-8'>
-        <Title title='The Evolution of My Tech Journey' />
+    return (
+        <div className='w-full px-64 max-[1285px]:px-52 max-lg:px-4 max-sm:px-5 flex flex-col items-center mt-4 pb-8'>
+            <Title title='The Evolution of My Tech Journey' />
 
-        <div className="w-full pl-36 pr-28 max-sm:px-2">
+            <div className="w-full pl-36 pr-28 max-sm:px-2">
 
-            {data.slice(0, 3).map((item, idx) => (
-                <div key={idx}>
-                    <h1 className={`text-2xl max-sm:text-xl mt-8 font-medium ${bricolage_grotesque}`}>{item.year}</h1>
-                    <div  className="flex pl-2 mt-4">               
-                        <div className={`w-full flex flex-col gap-3`}>
+                {data.slice(0, 3).map((item, idx) => (
+                    <div key={idx}>
+                        <h1 className={`text-2xl max-sm:text-xl mt-8 font-medium ${bricolage_grotesque}`}>{item.year}</h1>
+                        <div className="flex pl-2 mt-4">
+                            <div className={`w-full flex flex-col gap-3`}>
 
-                            {item.events.map((event, idx) => (
-                                <div key={idx} className={`flex items-center gap-2 ${bricolage_grotesque}`}>
-                                    <span><Separator orientation='horizontal' size='1' className='w-8 bg-black dark:bg-gray-400'/></span>
-                                    <span className='text-[15px] max-sm:text-sm dark:text-[#dfdede]'>{event}</span>
-                                </div>
-                            ))}
+                                {item.events.map((event, idx) => (
+                                    <div key={idx} className={`flex items-center gap-2 ${bricolage_grotesque}`}>
+                                        <span><Separator orientation='horizontal' size='1' className='w-8 bg-black dark:bg-gray-400' /></span>
+                                        <span className='text-[15px] max-sm:text-sm dark:text-[#dfdede]'>{event}</span>
+                                    </div>
+                                ))}
 
+                            </div>
                         </div>
                     </div>
+                ))}
+
+                <div className={`flex justify-center mt-5 ${isMore ? 'hidden' : 'block'}`}>
+                    <Badge color="gray" variant="solid" highContrast onClick={() => setIsMore(true)} className={`text-xs max-sm:text-[10px] w-20 flex items-center text-center dark:hover:bg-gray-300 py-1 px-2 cursor-pointer hover:bg-gray-800 ${bricolage_grotesque}`}>
+                        <span>See More</span>
+                        <span className='!ml-[-3px] mt-[1px]'>
+                            <ChevronDownIcon className='h-3 w-3 dark:!text-black !text-white  shrink-0 text-muted-foreground transition-transform duration-200' />
+                        </span>
+                    </Badge>
                 </div>
-            ))}
 
-            <div className={`flex justify-center mt-5 ${isMore ? 'hidden' : 'block'}`}>
-                <Badge color="gray" variant="solid" highContrast onClick={() => setIsMore(true)} className={`text-xs max-sm:text-[10px] w-20 flex items-center text-center dark:hover:bg-gray-300 py-1 px-2 cursor-pointer hover:bg-gray-800 ${bricolage_grotesque}`}>
-                    <span>See More</span>
-                    <span className='!ml-[-3px] mt-[1px]'>
-                        <ChevronDownIcon className='h-3 w-3 dark:!text-black !text-white  shrink-0 text-muted-foreground transition-transform duration-200'/>
-                    </span>
-                </Badge>
-            </div>
+                {
+                    isMore && data.slice(3).map((item, idx) => (
+                        <div key={idx}>
+                            <h1 className={`text-2xl mt-8 font-medium ${bricolage_grotesque}`}>{item.year}</h1>
+                            <div className="flex pl-2 mt-4">
+                                <div className={`w-full flex flex-col gap-3 ${bricolage_grotesque}`}>
 
-            {
-                isMore && data.slice(3).map((item, idx) => (
-                <div key={idx}>
-                    <h1 className={`text-2xl mt-8 font-medium ${bricolage_grotesque}`}>{item.year}</h1>
-                    <div className="flex pl-2 mt-4">               
-                        <div className={`w-full flex flex-col gap-3 ${bricolage_grotesque}`}>
+                                    {item.events.map((event, idx) => (
+                                        <div key={idx} className={`flex items-center gap-2 ${bricolage_grotesque}`}>
+                                            <span><Separator orientation='horizontal' size='1' className='w-8 bg-black dark:bg-gray-400' /></span>
+                                            <span className='text-[15px] dark:text-[#dfdede]'>{event}</span>
+                                        </div>
+                                    ))}
 
-                            {item.events.map((event, idx) => (
-                                <div key={idx} className={`flex items-center gap-2 ${bricolage_grotesque}`}>
-                                    <span><Separator orientation='horizontal' size='1' className='w-8 bg-black dark:bg-gray-400'/></span>
-                                    <span className='text-[15px] dark:text-[#dfdede]'>{event}</span>
                                 </div>
-                            ))}
-
+                            </div>
                         </div>
-                    </div>
-                </div>
-                ))
-            }
-            
+                    ))
+                }
 
-            <div className={`flex justify-center mt-5 ${isMore ? 'block' : 'hidden'}`}>
-                <Badge color="gray" variant="solid" highContrast onClick={() => setIsMore(false)} className={`text-xs max-sm:text-[10px] w-20 flex items-center text-center dark:hover:bg-gray-300 py-1 px-2 cursor-pointer hover:bg-gray-800 ${bricolage_grotesque}`}>
-                    <span>See Less</span>
-                    <span className='!ml-[-3px] mt-[1px]'>
-                        <ChevronDownIcon className='rotate-180 h-3 w-3 dark:!text-black !text-white shrink-0 text-muted-foreground transition-transform duration-200'/>
-                    </span>
-                </Badge>
+
+                <div className={`flex justify-center mt-5 ${isMore ? 'block' : 'hidden'}`}>
+                    <Badge color="gray" variant="solid" highContrast onClick={() => setIsMore(false)} className={`text-xs max-sm:text-[10px] w-20 flex items-center text-center dark:hover:bg-gray-300 py-1 px-2 cursor-pointer hover:bg-gray-800 ${bricolage_grotesque}`}>
+                        <span>See Less</span>
+                        <span className='!ml-[-3px] mt-[1px]'>
+                            <ChevronDownIcon className='rotate-180 h-3 w-3 dark:!text-black !text-white shrink-0 text-muted-foreground transition-transform duration-200' />
+                        </span>
+                    </Badge>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default About
 
 
-const data : I_About[] = [
+const data: I_About[] = [
+    {
+        year: 2025,
+        events: [
+            "Building something new this year—stay tuned."
+        ]
+    },
     {
         year: 2024,
         events: [
-            "Dove deep into Web Development, mastering cutting-edge technologies and building several innovative projects (and counting). 🚀",
+            "Dove deep into Web Development, mastered cutting-edge technologies and builded more the 20 projects. 🚀",
             "Started my journey in Web3 development, creating decentralized applications and embracing blockchain technology. ⛓",
-            "Proud of the progress I've made so far—each step has been a leap forward in my career."
+            "Got funded by Adobe for an project and Proud of the progress I've made so far—each step has been a leap forward in my career."
         ]
     },
     {
