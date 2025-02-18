918e2322ea612b31feadc0e0f8ce22ee5aebbb9c
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2025-02-18 19:41:13 +0530

    feat: added cal meet link

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { Link } from '@radix-ui/themes'
-import { FaWhatsapp } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { bricolage_grotesque } from '@/utils/fonts';
 import { Input } from "@/components/ui/input"
@@ -10,6 +9,7 @@ import { Button } from './ui/button';
 import axios, { AxiosError } from 'axios';
 import { ApiResponse } from '@/types/project';
 import { toast } from 'sonner'
+import { SiLivechat } from "react-icons/si";
 import Title from './ui/Title';
 
 
@@ -51,53 +51,53 @@ const Contact = () => {
             toast.error(error.message);
         } finally {
             setIsEmailSending(false);
-    }
-};
+        }
+    };
 
-  return (
-    <div className='w-full px-64 max-[1285px]:px-52 max-lg:px-4 max-sm:px-2 flex flex-col items-center mt-6 pb-8'>
-        <Title title='Get in Touch'/>
+    return (
+        <div className='w-full px-64 max-[1285px]:px-52 max-lg:px-4 max-sm:px-2 flex flex-col items-center mt-6 pb-8'>
+            <Title title='Get in Touch' />
 
-        <div className={`w-full flex flex-col gap-3 mt-6 px-36 max-sm:px-4 ${bricolage_grotesque}`}>
-            <div>
-                <h2 className='text-lg max-sm:text-base'>Have a Question? Let&apos;s Chat!</h2>
-            </div>
-            <div className='flex gap-3 mt-2'>
-                <Link href="https://wa.me/916377692757?text=Hello,%20I%20would%20like%20to%20chat%20with%20you!" target="_blank">
-                    <button className='bg-[#4ADE80] text-black py-2 px-3 rounded-md flex items-center gap-2 text-sm max-sm:text-xs hover:bg-[#42bc6f]'>
-                        <FaWhatsapp className='h-5 w-5'/> Chat on WhatsApp
-                    </button>
-                </Link>
+            <div className={`w-full flex flex-col gap-3 mt-6 px-36 max-sm:px-4 ${bricolage_grotesque}`}>
+                <div>
+                    <h2 className='text-lg max-sm:text-base'>Have a Question? Let&apos;s Chat!</h2>
+                </div>
+                <div className='flex gap-3 mt-2'>
+                    <Link href="https://cal.com/fardeen26/15min" target="_blank">
+                        <button className='bg-[#4ADE80] text-black py-2 px-3 rounded-md flex items-center gap-2 text-sm max-sm:text-xs hover:bg-[#42bc6f]'>
+                            <SiLivechat className='h-[18px] w-[18px]' /> Book a meet
+                        </button>
+                    </Link>
 
-                <Link href="https://twitter.com/messages/compose?recipient_id=1596464580753911810" target="_blank">
-                    <button className='bg-[#1D9BF0] text-white py-2 px-3 rounded-md flex items-center gap-2 text-sm max-sm:text-xs hover:bg-[#2e7bae]'>
-                    <FaXTwitter className='h-[18px] w-[18px]'/> Chat on Twitter
-                    </button>
-                </Link>
-            </div>
+                    <Link href="https://twitter.com/messages/compose?recipient_id=1596464580753911810" target="_blank">
+                        <button className='bg-[#1D9BF0] text-white py-2 px-3 rounded-md flex items-center gap-2 text-sm max-sm:text-xs hover:bg-[#2e7bae]'>
+                            <FaXTwitter className='h-[18px] w-[18px]' /> Chat on Twitter
+                        </button>
+                    </Link>
+                </div>
 
-            <div className='mt-6'>
-                <h2 className='text-lg text-start max-sm:text-base'>Reach Out via Email</h2>
-            </div>
+                <div className='mt-6'>
+                    <h2 className='text-lg text-start max-sm:text-base'>Reach Out via Email</h2>
+                </div>
 
-            <div className="w-full flex justify-center">
-                <div className="mt-4 w-full">
-                    <form className='flex flex-col gap-4' onSubmit={sendEmailMessage}>
-                        <div className="grid w-full gap-2">
-                            <Label htmlFor="message">Your Email</Label>
-                            <Input type="text" className='w-full h-full border' placeholder='johndoe69@xyz.com' value={email} onChange={(e) => setEmail(e.target.value)} required/>
-                        </div>
-                        <div className="grid w-full gap-2">
-                            <Label htmlFor="message">Your message</Label>
-                            <Textarea placeholder="Type your message here." id="message" value={message} onChange={(e) => setMessage(e.target.value)} required minLength={5}/>
-                        </div>
-                        <Button className='mt-3'>{isEmailSending ? 'Sending Message...' : 'Send message'}</Button>
-                    </form>
+                <div className="w-full flex justify-center">
+                    <div className="mt-4 w-full">
+                        <form className='flex flex-col gap-4' onSubmit={sendEmailMessage}>
+                            <div className="grid w-full gap-2">
+                                <Label htmlFor="message">Your Email</Label>
+                                <Input type="text" className='w-full h-full border' placeholder='johndoe69@xyz.com' value={email} onChange={(e) => setEmail(e.target.value)} required />
+                            </div>
+                            <div className="grid w-full gap-2">
+                                <Label htmlFor="message">Your message</Label>
+                                <Textarea placeholder="Type your message here." id="message" value={message} onChange={(e) => setMessage(e.target.value)} required minLength={5} />
+                            </div>
+                            <Button className='mt-3'>{isEmailSending ? 'Sending Message...' : 'Send message'}</Button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Contact
