84b89dbe4cdaedaedbcaf0fa2e05ec0141ecf434
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2025-04-28 23:18:11 +0530

    feat: added load more button in project list

--- src/app/projects/components/ProjectCardList.tsx
+++ src/app/projects/components/ProjectCardList.tsx
@@ -3,17 +3,25 @@
 import { Project } from '@/types/project';
 import ProjectCard from './ProjectCard';
 import Title from '@/components/ui/Title';
-// import SimpleProjectCard from '@/components/SimpleProjectCard';
+import { useState } from 'react';
+import { ChevronDownIcon } from '@radix-ui/react-icons';
+import { Badge } from '@radix-ui/themes';
+import { bricolage_grotesque } from '@/utils/fonts';
 
 
 const ProjectCardList = () => {
+  const [visibleProjects, setVisibleProjects] = useState(6);
+
+  const loadMoreProjects = () => {
+    setVisibleProjects((prev) => prev + 6);
+  };
+
   return (
     <div className='w-full h-fit px-64 max-[1025px]:px-4 max-[1285px]:px-40 max-lg:px-0 max-sm:px-4 flex flex-col items-center mt-4 pb-8'>
       <Title title='Proof of Work' />
-      {/* <SimpleProjectCard /> */}
 
       <div className="flex w-full flex-col gap-4 lg:flex-row mt-4 px-32 max-lg:px-0 max-sm:px-0 flex-wrap items-center ml-14 max-sm:ml-0 max-lg:ml-0 max-[350px]:mr-5 max-[321px]:mr-10">
-        {data.map((project: Project, idx: number) => (
+        {data.slice(0, visibleProjects).map((project: Project, idx: number) => (
           <ProjectCard
             key={idx}
             logo={project.logo}
@@ -25,6 +33,14 @@ const ProjectCardList = () => {
           />
         ))}
       </div>
+      {visibleProjects < data.length && (
+        <Badge color="gray" variant="solid" highContrast onClick={loadMoreProjects} className={`text-xs max-sm:text-[10px] flex items-center text-center dark:hover:bg-gray-300 py-1 px-2 cursor-pointer hover:bg-gray-800 mt-6 ${bricolage_grotesque}`}>
+          <span>Load More</span>
+          <span className='!ml-[-3px] mt-[1px]'>
+            <ChevronDownIcon className='h-3 w-3 dark:!text-black !text-white  shrink-0 text-muted-foreground transition-transform duration-200' />
+          </span>
+        </Badge>
+      )}
     </div>
   )
 }
--- src/components/ui/Title.tsx
+++ src/components/ui/Title.tsx
@@ -6,7 +6,7 @@ import { bricolage_grotesque } from '@/utils/fonts';
 
 const Title = ({ title }: I_Heading) => {
   return (
-    <div className='px-10 mb-4'>
+    <div className='px-10'>
       <h1 className={`text-[1.6rem] max-sm:text-2xl font-semibold tracking-tight text-start max-sm:text-center ${bricolage_grotesque}`}>
         {title}
       </h1>
